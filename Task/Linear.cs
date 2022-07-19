f168ee4a1467e1b75e6a7cbeaf4f4f8e86336e00
lllypik <blesphoton@gmail.com>
2022-07-19 21:18:09 +0300

    Добавьте файлы проекта.

---
+++ Task/Linear.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    struct Linear
+    {
+        double factorB;
+        double factorK;
+
+
+        public Linear(double factorB, double factorK)
+        {
+            this.factorB = factorB;
+            this.factorK = factorK;
+        }
+
+        public double Root()
+        {
+            return -factorB / factorK;
+        }
+        public string Note()
+        {
+            if (factorK == 0) return "Деление на 0! Не является ошибкой";
+            else return "Замечания отсутствуют";
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты В и К из уравнения k*Х + b = 0");
+            double coeffB = Convert.ToDouble(Console.ReadLine());
+            double coeffK = Convert.ToDouble(Console.ReadLine());
+
+            Linear equation = new Linear(coeffB, coeffK);
+
+            Console.WriteLine("Решение данного уравнения: Х = {0} \n{1}", equation.Root(), equation.Note ());
+            Console.ReadKey();
+
+        }
+    }
+}
+
